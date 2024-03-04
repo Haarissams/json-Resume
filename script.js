9c792bdf5dce2a31956c94ce846e0f62a8fa399b
Haarissams <haarissams02@gmail.com>
2024-03-04 20:16:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const resume = {
+    "name" :  "Mohamed Haaris S",
+     "email" : "haarissams02@gmail.com",
+    "phone": "8610603114",
+    "address": "Cumbummetu Road Cumbum ,Theni",
+    "summary": "Pursuing MCA in Jamal Mohamed College",
+    "education": [
+      {
+        "degree": "Bachelor of Science in Computer Science",
+        "university": "HKRH",
+        "year": "20-2022"
+      }
+    ],
+    
+    "SKILLS" : [
+        {
+          "languages":  "javascript,  python,  react.js",
+           "Knowledge In" : "Front-End Devolopment and Designing"
+  }  ]
+  }
+  console.log(resume);
+  
